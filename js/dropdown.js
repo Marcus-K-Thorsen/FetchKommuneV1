ceb9b1cd647a69f56fdc4e2c19f3cac1e469e185
Marcus <marc303i@stud.kea.dk>
2023-02-24 09:54:28 +0100

    indført dropdown menu med frugter og en nød, som sætter sin klasse og ændrer sin style

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,10 +1,10 @@
 console.log("Jeg er i dropdown.js")
 
 const fruits = []
-fruits.push({"name": "æble", "style": "fruit"})
-fruits.push({"name": "pære", "style": "fruit"})
-fruits.push({"name": "peanut", "style": "notfruit"})
-fruits.push({"name": "appelsin", "style": "fruit"})
+fruits.push({"id": 1, "name": "æble", "style": "fruit"})
+fruits.push({"id": 2, "name": "pære", "style": "fruit"})
+fruits.push({"id": 3, "name": "peanut", "style": "nut"})
+fruits.push({"id": 4, "name": "appelsin", "style": "fruit"})
 
 console.log(fruits)
 
@@ -15,11 +15,23 @@ function addFruitToDropDown(item) {
     console.log(item)
     const el = document.createElement("option")
     el.textContent = item.name
+    el.className = item.style
     ddFruits.appendChild(el)
+    //el.value = item.id , puts primary key in value
+    el.value = item // puts object in value
 }
 
 function fillDropDown() {
     fruits.forEach(addFruitToDropDown)
 }
 
+function selectFruit() {
+    const selindex = ddFruits.selectedIndex
+    const selectedFruit = ddFruits.options[selindex]
+    console.log(selindex)
+    console.log(selectedFruit)
+    console.log(selectedFruit.value)
+}
+
 pbFillDropDown.addEventListener('click', fillDropDown)
+ddFruits.addEventListener('change', selectFruit)
